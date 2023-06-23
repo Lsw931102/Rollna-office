f36ad7e0f035851495fc0a5a1ca8742783117df6
Funny <736907735@qq.com>
2023-06-23 18:08:17 +0800

    change

--- src/utils/token.ts
+++
@@ -1,7 +0,0 @@
-import getConfig from 'next/config'
-
-const { publicRuntimeConfig } = getConfig()
-
-export function getTokenDecimal(key: string) {
-  return publicRuntimeConfig?.decimals?.[key.toLocaleUpperCase()] ?? 18
-}
